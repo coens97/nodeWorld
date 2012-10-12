52dd4396ee99f5aeb4002cadedd6336b71a560c1
Coen <coen.stange@hotmail.com>
2012-10-12 13:08:38 -0400

    Changed port a bit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 rootDir = __dirname;//so we now what is the root directory in other files
+appPort = process.env.PORT;
 
 var requirejs = require('requirejs');//so we could include files in project the same way the client does
 
--- server/httpServer.js
+++ server/httpServer.js
@@ -2,4 +2,4 @@ var Connect = require('./connect');
 Connect.createServer(
 	//Connect.logger(), // Log responses to the terminal using Common Log Format.
 	Connect.static(rootDir+"/client/")
-).listen(8080);
+).listen(appPort);
