dae2623dca14a3df281e689306a2b21b62ffa5c3
Coen Stange <coenstange@me.com>
2012-10-12 19:22:04 +0200

    Changed port again

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 rootDir = __dirname;//so we now what is the root directory in other files
-appPort = process.env.PORT;
+appPort = (process.env.PORT===undefined)?3000:process.env.PORT;//Do port 80 when it's not defined in node
 
 var requirejs = require('requirejs');//so we could include files in project the same way the client does
-
-requirejs.config({
-    nodeRequire: require
-});
 
 requirejs(['server/httpServer']);
--- server/httpServer.js
+++ server/httpServer.js
@@ -2,4 +2,4 @@ var Connect = require('./connect');
 Connect.createServer(
 	//Connect.logger(), // Log responses to the terminal using Common Log Format.
 	Connect.static(rootDir+"/client/")
-).listen(appPort);
+).listen(appPort,function(){console.log("HTTP server running on "+appPort);});
