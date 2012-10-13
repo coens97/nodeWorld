9fa200326a92aed28aadc2cd9e085c539f7dbdc7
Coen <coen.stange@hotmail.com>
2012-10-13 15:18:15 -0400

    Updated scenes a little bit

--- client/js/game.js
+++ client/js/game.js
@@ -7,10 +7,11 @@ var game = {
         new intro()//0
     ],
     loop : function(){//gameLoop function(game.loop)
-
+        this.scenes[this.currentScene].loop();
     },
     draw : function(){
-
+        ctx.clearRect (0,0,800,600);//clear the canvas
+        this.scenes[this.currentScene].draw();
     },
     mouseDown : function(x,y){//resolution 1280 720
         console.log("Mouse down x:"+x+"y:"+y);
