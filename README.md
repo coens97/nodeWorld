5510dfbebb6e165c9c38437d2e65d199dbbe14ce
Coen <coen.stange@hotmail.com>
2012-10-13 12:10:24 -0400

    Changed http server

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Getting started
 * First clone this project
 * Then in install nodeJS
-* Run this in commandline `npm install websocket connect requirejs`
+* Run this in commandline `npm install websocket mime requirejs`
 * Run serer `node app.js`
 
 ## Optimize js files
--- server/httpServer.js
+++ server/httpServer.js
@@ -1,5 +1,27 @@
-var Connect = require('./connect');
-Connect.createServer(
-	//Connect.logger(), // Log responses to the terminal using Common Log Format.
-	Connect.static(rootDir+"/client/")
-).listen(config.port,function(){console.log("HTTP server running on "+config.port);});
+var app = require('http').createServer(handler),
+  main = require('./server/main'),
+  fs = require('fs'),
+  mime = require('mime'),
+  url = require('url');
+ 
+app.listen(config.port);
+//http server
+function handler (req, res) {
+    console.log("Page loading:"+url.parse(req.url).href);
+    var theUrl;
+	if(url.parse(req.url).href=="/"){
+		theUrl = "index.html";
+	}
+	else{
+		theUrl = url.parse(req.url).href;
+	}
+  fs.readFile(rootDir + '/client/'+theUrl,
+  function (err, data) {
+    if (err) {
+	  res.writeHead(500);
+      return res.end('Whazup dude\nJammer de pagina laadt niet');
+    }
+    res.writeHead(200, {'Content-Type':mime.lookup(theUrl)});
+    res.end(data);
+  });
+}
