8a9f684c55219bc4acdce94cd61286b2e4f9aa5f
Coen Stange <coenstange@me.com>
2012-10-14 15:33:17 +0200

    Changed lincense

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE FRIES LICENSE":
+ * Coen wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me some fries
+ * ----------------------------------------------------------------------------
+ */
 console.log("Starting server");//say starting server
 rootDir = __dirname;//so we now what is the root directory in other files
 config = require('./config');//load config file
