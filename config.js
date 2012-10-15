2108794be96c74206f8fdc92d945bca83d0d68d5
Coen Stange <coenstange@me.com>
2012-10-15 10:16:35 +0200

    Changed port configuration

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-this.port = (process.env.PORT===undefined)?8080:process.env.PORT;//Do port 80 when it's not defined in node
+this.port = process.env.PORT||8080;//Do port 80 when it's not defined in node
 this.logHHTP = false;
 
