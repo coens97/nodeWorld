b61a3edbe3722cf0df823321a87e978c611f4def
Coen Stange <coenstange@me.com>
2012-10-15 21:22:16 +0200

    Updated the server a bit

--- server/game.js
+++ server/game.js
@@ -2,21 +2,27 @@
  * game.js
  ****************/
 var stats = require("./stats");
-this.connections = 0;
  
 this.newConnection = function(socket){
-	//here must al the user specific stuff
-	console.log("New connection "+ ++stats.connections);//also add 1 connection to the count 
-	
-	
+	//here must al the user specific stuff	
 	socket.on('echo', function (data) {//for debugging
 		console.log("Echo:"+data);
 		socket.emit('echo','Echo:'+data);
 	});
 	
-	socket.on('disconnect', this.disconnect);
+	this.playerCount = function(){
+		socket.broadcast.emit("playerCount",stats.connections);
+		socket.emit("playerCount",stats.connections);
+	};
+	this.disconnected = function(){
+		console.log("Somebody disconnected "+stats.connections);
+		stats.connections--;		
+	};
+	socket.on('disconnect',this.disconnected);
+	//new user
+	console.log("New connection "+ ++stats.connections);//also add 1 connection to the count 
+	socket.broadcast.emit("playerCount",stats.connections);
+	this.playerCount();
 }
-this.disconnect = function(){
-	console.log("Somebody disconnected "+this.connections);
-	stats.connections--;
-}
+
+
