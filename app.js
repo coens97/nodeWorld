fd549442c9f127d61f123ec233ae0c290b52194f
Coen Stange <coenstange@me.com>
2012-10-15 15:40:17 +0200

    Now tells when starting serverr which port it uses

--- app.js
+++ app.js
@@ -6,8 +6,9 @@
  * this stuff is worth it, you can buy me some fries
  * ----------------------------------------------------------------------------
  */
-console.log("Starting server");//say starting server
 rootDir = __dirname;//so we now what is the root directory in other files
 config = require('./config');//load config file
+
+console.log("Starting server on "+config.port);//say starting server
 
 require("./server/main");//start server
