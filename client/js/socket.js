a7550abd53e5db10ac87d76b24bbbdd683df4053
Coen Stange <coenstange@me.com>
2012-10-15 15:35:25 +0200

    Added conection indicator in client

--- client/js/socket.js
+++ client/js/socket.js
@@ -3,7 +3,9 @@ var socket = io.connect('http://'+window.location.host);
 
 socket.on('connect', function () {
 	console.log("We have connection! :)");
+	document.getElementById("status").style.backgroundColor = "#00FF00";
 });
 socket.on('disconnect', function () {
 	console.log("Connection lost");
+	document.getElementById("status").style.backgroundColor = "#FF0000";
 });
