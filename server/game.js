1baeedec557144fb7a0ea8ae2dec40a99974a306
Coen Stange <coenstange@me.com>
2012-10-15 19:04:00 +0200

    Add connection counter add server side

--- server/game.js
+++ server/game.js
@@ -1,19 +1,22 @@
 /*****************
  * game.js
  ****************/
+var stats = require("./stats");
 this.connections = 0;
  
 this.newConnection = function(socket){
 	//here must al the user specific stuff
-	console.log("New connection");
+	console.log("New connection "+ ++stats.connections);//also add 1 connection to the count 
+	
 	
 	socket.on('echo', function (data) {//for debugging
 		console.log("Echo:"+data);
 		socket.emit('echo','Echo:'+data);
 	});
 	
-	socket.on('disconnect', function () {
-		console.log("Somebody disconnected");
-	});
+	socket.on('disconnect', this.disconnect);
 }
-
+this.disconnect = function(){
+	console.log("Somebody disconnected "+this.connections);
+	stats.connections--;
+}
