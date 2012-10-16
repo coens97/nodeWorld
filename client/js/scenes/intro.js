ca492d6615d0965c104ff1f393ffbdebd4499297
Coen Stange <coenstange@me.com>
2012-10-16 14:58:26 +0200

    Added Coen Stange sprite

--- client/js/scenes/intro.js
+++ client/js/scenes/intro.js
@@ -1,7 +1,8 @@
 function intro(){
     this.sprites = {
         bg : new rect("DDDDDD",0,0,1280,720),
-		title : new image("images/title.png",145,25,0,0)//dimension of title is 921X221
+		title : new image("images/title.png",145,25,0,0),//dimension of title is 921X221
+		coen : new image("images/coen.png",1280,500,620,160)//dimension 620x160
     };
     this.loop = function(){
 		//Start the animation of the title
@@ -9,6 +10,8 @@ function intro(){
 			this.sprites.title.w +=18.4;//make the image bigger
 			this.sprites.title.h +=4.4;//make the image higher
 			this.sprites.title.x = 640 - this.sprites.title.w / 2;//puth the image exact in the middle
+		}else if(this.sprites.coen.x > 500){//move the Coen sprite
+			this.sprites.coen.x -= 20;
 		}
     };
     this.draw = function(){
