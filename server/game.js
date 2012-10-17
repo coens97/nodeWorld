769c36c76fc4e61fe71e550c3143a4fbcf34ba03
Coen Stange <coenstange@me.com>
2012-10-17 18:40:07 +0200

    Updated rooms again

--- server/game.js
+++ server/game.js
@@ -58,9 +58,10 @@ exports.newConnection = function(socket){
 			}
 			console.log(this.nickname+" tries to host a game");
 			console.log("Room:"+data.name+" laps:"+data.laps);
+			
 			room.rooms[data.name] = new room.room(data.laps);//create the room
-			room.rooms[data.name].players[this.nickname] = players[this.nickname];//add player to room
-			this.player.state = 2;
+			room.rooms[data.name].addPlayer(this.nickname,players[this.nickname]);//add player to scene
+
 			socket.emit('hostGame',1);//say to the client it's ok
 			//send to the other client this room is available
 			emitRooms();
@@ -71,8 +72,7 @@ exports.newConnection = function(socket){
 	};	
 	this.toRoom = function(data){
 		console.log(this.nickname+" wants to join "+data);
-		this.player.state = 2;
-		room.rooms[data].players[this.nickname] = players[this.nickname];//add player to scene
+		room.rooms[data].addPlayer(this.nickname,players[this.nickname]);//add player to scene
 		socket.emit('toRoom',1);
 		emitRooms();//because the number of players have changed
 	}
--- server/room.js
+++ server/room.js
@@ -1,8 +1,14 @@
 this.rooms = {};
 
 this.room = function(laps){
+	this.state = 0;//waiting
 	this.laps = laps;
 	this.players = {};
+	this.addPlayer = function(nickname,player){//when player goes to room
+		this.players[nickname] = player;
+		this.player = this.players[nickname];
+		this.player.state = 2;//scene state
+	};
 }
 
 this.sendRooms = function(socket){
