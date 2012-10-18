08445c47d80ce674efb0919f2bb2a475a7e8fed8
Coen Stange <coenstange@me.com>
2012-10-18 17:45:36 +0200

    Added cars sprites

---
+++ client/js/scenes/gameRoom.js
@@ -0,0 +1,28 @@
+function gameRoom(){
+    this.sprites = {
+        bg : new vGrad({0:"#939393",0.1:"#DDDDDD",0.9:"#DDDDDD",1:"#939393"},0,0,1280,720),
+    };
+	this.startScene = function(){
+		
+	};
+	this.stopScene = function(){
+		
+	};
+    this.loop = function(){
+
+    };
+    this.draw = function(){
+        /***********
+         * loop trough all sprite and draw it on canvas
+         ***********/     
+        for(var thisSprite in this.sprites){
+            this.sprites[thisSprite].draw();       
+        } 
+    };
+    this.mouseDown = function(x,y){
+        
+    };
+    this.keyDown = function(key){
+    
+    };
+}
