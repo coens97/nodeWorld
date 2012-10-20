1e997a0c24fa9740cdbe05b2e265e2bc40e2f24c
Coen Stange <coenstange@me.com>
2012-10-20 22:16:52 +0200

    Collision doesn't work well yet

--- client/js/sprites/player.js
+++ client/js/sprites/player.js
@@ -12,7 +12,7 @@ function player(x,y){
 	this.draw = function(){
 		ctx.fillStyle = this.color;
 		ctx.beginPath();
-		ctx.arc(this.x+32, this.y+32, 32, 0, Math.PI*2, true); 
+		ctx.arc(this.x+32, this.y+32, 31, 0, Math.PI*2, true); 
 		ctx.closePath();
 		ctx.fill();
 		ctx.lineWidth = 2;
@@ -26,13 +26,17 @@ function currentPlayer(x,y,world,scene){
 		this.p.draw();
 	};
 	this.loop = function(){
-		var aX = Math.round(scene.pX/world.tilesets[0].tilewidth);
+		var aX = Math.round(scene.pX/world.tilesets[0].tilewidth-0.5);
 		var aY = Math.round(scene.pY/world.tilesets[0].tileheight-0.5);
 		var cB = world.layers[0].data[(aY+1)*world.layers[0].width+aX];
-		if(cB!=0&&def(cB)&&scene.vY>0){//if there is an object under and
+		if(cB!=0&&def(cB)&&scene.vY>0){//if there is an object under and moving down
 			scene.vY = 0;
 		}
-		console.log(aX+" "+aY+" "+cB);
+		var cU = world.layers[0].data[(aY)*world.layers[0].width+aX];
+		if(cU!=0&&def(cU)&&scene.vY<0){//if there is an object above and moving up
+			scene.vY = 0;
+		}
+		//console.log(aX+" "+aY+" "+cB);
 		scene.pX += scene.vX;
 		scene.pY += scene.vY;
 	};
