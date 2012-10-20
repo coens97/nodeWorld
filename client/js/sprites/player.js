f8354df4ac57f75026ea5f61706f17ca3af710c3
Coen Stange <coenstange@me.com>
2012-10-20 18:24:28 +0200

    Changed colors

---
+++ client/js/sprites/player.js
@@ -0,0 +1,15 @@
+function player(x,y){
+	this.x = x;
+	this.y = y;
+	this.color = "#59E01B";
+	this.draw = function(){
+		ctx.fillStyle = this.color;
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, 32, 0, Math.PI*2, true); 
+		ctx.closePath();
+		ctx.fill();
+		ctx.lineWidth = 2;
+        ctx.strokeStyle = "#000000";
+        ctx.stroke();
+	};
+}
