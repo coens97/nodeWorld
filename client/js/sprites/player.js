94cb511a00a78833f3688682e068067e1d6e9d42
Coen Stange <coenstange@me.com>
2012-10-21 03:06:58 +0200

    Changed a lot

--- client/js/sprites/player.js
+++ client/js/sprites/player.js
@@ -31,78 +31,80 @@ function player(x,y,world,scene){
         ctx.strokeStyle = "#000000";
         ctx.stroke();
 	};
+	this.getBound = function(){
+		return{a1X : Math.round((scene.pX-8)/world.tilesets[0].tilewidth-0.5),//xpos left from it
+				a2X : Math.round(scene.pX/world.tilesets[0].tilewidth),//xpos of it
+				a3X : Math.round(scene.pX/world.tilesets[0].tilewidth+0.5),//xpos right of it
+				a1Y :  Math.round((scene.pY)/world.tilesets[0].tileheight-0.5),//ypos of above it
+				a2Y : Math.round((scene.pY)/world.tilesets[0].tileheight),//ypos of it
+				a3Y : Math.round((scene.pY)/world.tilesets[0].tileheight+0.5)}//ypos under it
+	};
 	this.loop = function(){
 		this.vY +=1;
-		scene.pX += this.vX;
-		scene.pY += this.vY;
 		//collision
-		var a1X = Math.round((scene.pX-8)/world.tilesets[0].tilewidth-0.5);//xpos left from it
-		var a2X = Math.round(scene.pX/world.tilesets[0].tilewidth);//xpos of it
-		var a3X = Math.round(scene.pX/world.tilesets[0].tilewidth+0.5);//xpos right of it
-
-		var a1Y =  Math.round((scene.pY)/world.tilesets[0].tileheight-0.5);
-		var a2Y = Math.round((scene.pY)/world.tilesets[0].tileheight);//ypos of it
-		var a3Y = Math.round((scene.pY)/world.tilesets[0].tileheight+0.5);//ypos under it
-		var aY = Math.round((scene.pY)/world.tilesets[0].tileheight);//ypos of it
+		var bD = this.getBound();
 		//vertical collision
-		if(a1X+2==a3X){//if horizontaly align to grid
+		if(bD.a1X+2==bD.a3X){//if horizontaly align to grid
 			if(this.vY<0){//if moving up
-				var cU = world.layers[0].data[(a1Y)*world.layers[0].width+a2X];
+				var cU = world.layers[0].data[(bD.a1Y)*world.layers[0].width+bD.a2X];
 				if(checkCol(cU)){//if there is an object above and moving up
-					scene.pY = aY*world.tileheight;
+					scene.pY = bD.a2Y*world.tileheight;
 					this.vY = 0;
 				}
 			}else if(this.vY>0){//if moving down
-				var cB = world.layers[0].data[(a3Y)*world.layers[0].width+a2X];
+				var cB = world.layers[0].data[(bD.a3Y)*world.layers[0].width+bD.a2X];
 				if(checkCol(cB)){
-					scene.pY = aY*world.tileheight;
+					scene.pY = bD.a2Y*world.tileheight;
 					this.vY = 0;
 				}
 			}
 		}else{
 			if(this.vY<0){//if moving up
-				var c1U = world.layers[0].data[(a1Y)*world.layers[0].width+a1X];
-				var c2U = world.layers[0].data[(a1Y)*world.layers[0].width+a3X];
+				var c1U = world.layers[0].data[(bD.a1Y)*world.layers[0].width+bD.a1X];
+				var c2U = world.layers[0].data[(bD.a1Y)*world.layers[0].width+bD.a3X];
 				if(checkCol(c1U)||checkCol(c2U)){//if there is an object above and moving up
-					scene.pY = aY*world.tileheight;
+					scene.pY = bD.a2Y*world.tileheight;
 					this.vY = 0;
 				}
 			}else if(this.vY>0){//if moving down
-				var c1B = world.layers[0].data[(a3Y)*world.layers[0].width+a1X];
-				var c2B = world.layers[0].data[(a3Y)*world.layers[0].width+a3X];
+				var c1B = world.layers[0].data[(bD.a3Y)*world.layers[0].width+bD.a1X];
+				var c2B = world.layers[0].data[(bD.a3Y)*world.layers[0].width+bD.a3X];
 				if(checkCol(c1B)||checkCol(c2B)){
-					scene.pY = aY*world.tileheight;
+					scene.pY = bD.a2Y*world.tileheight;
 					this.vY = 0;
 				}
 			}
 		}
+		bD = this.getBound();
 		//horizontal collision
-		if(a1Y+2==a3Y){//if vericaly align to grid
+		if(bD.a1Y+2==bD.a3Y){//if vericaly align to grid
 			if(this.vX<0){//if moving to left
-				var cL = world.layers[0].data[(a2Y)*world.layers[0].width+a1X];
+				var cL = world.layers[0].data[(bD.a2Y)*world.layers[0].width+bD.a1X];
 				if(checkCol(cL)){
 					this.vX = 0;
 				}
 			}else if(this.vX>0){//or when moving right
-				var cR = world.layers[0].data[(a2Y)*world.layers[0].width+a3X];
+				var cR = world.layers[0].data[(bD.a2Y)*world.layers[0].width+bD.a3X];
 				if(checkCol(cR)){
 					this.vX = 0;
 				}
 			}
 		}else{
 			if(this.vX<0){//if moving to left
-				var c1L = world.layers[0].data[(a1Y)*world.layers[0].width+a1X];
-				var c2L = world.layers[0].data[(a3Y)*world.layers[0].width+a1X];
+				var c1L = world.layers[0].data[(bD.a1Y)*world.layers[0].width+bD.a1X];
+				var c2L = world.layers[0].data[(bD.a3Y)*world.layers[0].width+bD.a1X];
 				if(checkCol(c1L)||checkCol(c2L)){
 					this.vX = 0;
 				}
 			}else if(this.vX>0){//or when moving right
-				var c1R = world.layers[0].data[(a1Y)*world.layers[0].width+a3X];
-				var c2R = world.layers[0].data[(a3Y)*world.layers[0].width+a3X];
+				var c1R = world.layers[0].data[(bD.a1Y)*world.layers[0].width+bD.a3X];
+				var c2R = world.layers[0].data[(bD.a3Y)*world.layers[0].width+bD.a3X];
 				if(checkCol(c1R)||checkCol(c2R)){
 					this.vX = 0;
 				}
 			}
 		}
+		scene.pX += this.vX;
+		scene.pY += this.vY;
 	};
 }
