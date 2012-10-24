f797d2ee58dba9b6a2dc6758897b11ccedb91820
Coen Stange <coenstange@me.com>
2012-10-24 13:32:19 +0200

    Added updates loop after evry three frame, will send the changes of players

--- server/gameRoom.js
+++ server/gameRoom.js
@@ -2,17 +2,23 @@
 this.gameRoom = function(parent){
 	var gameRoom = this;
 	this.players = [];
-	this.interval;
+	this.intervalG;//gameLoop
+	this.intervalG;//sendUpdates
 	this.startGame = function(){
-		this.interval = setInterval(this.gameLoop,1000/60);
+		this.intervalG = setInterval(this.gameLoop,1000/60);//60fps,16ms
+		this.intervalU = setInterval(this.sendUpdates,45);//45ms
 	};
 	this.addPlayer = function(player){
 		
 	};
 	this.stopGame = function(){
-		clearInterval(this.interval);
+		clearInterval(this.intervalG);
+		clearInterval(this.intervalU);
 	};
 	this.gameLoop = function(){
 		//console.log("Hello from gameLoop!");
+	};
+	this.sendUpdates = function(){
+		//console.log("Sending updates");
 	};
 };
