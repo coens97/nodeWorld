b950b364a7a0d42de0fd602392ed3acbab5e5fcf
Coen Stange <coenstange@me.com>
2012-10-27 22:39:49 +0200

    Changed names in intro

--- client/js/scenes/intro.js
+++ client/js/scenes/intro.js
@@ -2,7 +2,7 @@ function intro(){
     this.sprites = {
         bg : new vGrad({0:"#939393",0.1:"#DDDDDD",0.9:"#DDDDDD",1:"#939393"},0,0,1280,720),
 		title : new image("images/title.png",145,25,0,0),//dimension of title is 1095X336
-		coen : new image("images/coen.png",1280,500)//dimension 620x160
+		coen : new image("images/naam.png",1280,400)//dimension 612x200
     };
 	this.startScene = function(){
 		this.done = false;
@@ -16,7 +16,7 @@ function intro(){
 			this.sprites.title.w +=21.9;//make the image bigger
 			this.sprites.title.h +=6.72;//make the image higher
 			this.sprites.title.x = 640 - this.sprites.title.w / 2;//puth the image exact in the middle
-		}else if(this.sprites.coen.x > 500){//move the Coen sprite
+		}else if(this.sprites.coen.x > 334){//move the Coen sprite
 			this.sprites.coen.x -= 20;
 		}else if(!this.done){//animation is done now
 			this.done = true;
