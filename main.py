36cd34903c53e36106d4436ac72c91160e17bd0b
Coen Stange <coenstange@me.com>
2012-11-11 20:15:33 +0100

    Now saves things in logfile

--- main.py
+++ main.py
@@ -3,4 +3,4 @@
 #This script will restart the server when it crashes
 import subprocess
 while 1:
-	subprocess.Popen("node app.js", shell=True).wait()
+	subprocess.Popen("node app.js >> log.txt", shell=True).wait()
