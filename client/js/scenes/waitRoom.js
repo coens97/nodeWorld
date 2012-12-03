7c171b59a3f44e805d51a7109ad9ea4db570253e
Coen Stange <coenstange@me.com>
2012-12-03 22:37:25 +0100

    Added cool players

--- client/js/scenes/waitRoom.js
+++ client/js/scenes/waitRoom.js
@@ -27,9 +27,9 @@ function waitRoom(){
     this.sprites.pSpeed.font = "40pt Arial";
 
     //initialise trolls
-    for (var i = 0; i < 3; i++) {
-        this.sprites.trolsBg.ar.push(new roundRect("#B8B8B8",90+i*148, 320, 84, 148, 10));  
-        this.sprites.trols.ar[i] = new pImage(trollImg, 100+i*148,330, 64, 128, 0, i, 64, 128);      
+    for (var i = 0; i < 7; i++) {
+        this.sprites.trolsBg.ar.push(new roundRect("#B8B8B8",30+i*128, 320, 84, 148, 10));  
+        this.sprites.trols.ar[i] = new pImage(trollImg, 40+i*128,330, 64, 128, 0, i, 64, 128);      
      }
 
 	this.startScene = function(){
