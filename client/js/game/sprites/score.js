eb63ae2b05fb79e406e0917dfa79bdb5e4b6fda8
Coen Stange <coenstange@me.com>
2012-12-22 22:52:03 +0100

    Changed health in gameplay from bar to circle

--- client/js/game/sprites/score.js
+++ client/js/game/sprites/score.js
@@ -4,9 +4,20 @@
 function score() {
 	this.health = 100;
 	this.draw = function(){
-		ctx.fillStyle = "#DDDDDD";
-   		ctx.fillRect(40, 40, 154, 14);
-   		ctx.fillStyle = "#FF0000";
-   		ctx.fillRect(42, 42, 1.5*this.health, 10);
+		//////show health
+		//draw outer circle
+		ctx.beginPath();
+		ctx.strokeStyle = "#DDDDDD";
+		ctx.lineWidth = 10;
+   		ctx.arc(120,620,70,0,2*Math.PI);
+   		ctx.stroke();
+   		ctx.closePath();
+   		//draw health
+   		ctx.beginPath();
+   		ctx.strokeStyle = "#FF0000";
+   		ctx.lineWidth = 8;
+   		ctx.arc(120,620,70,1.5*Math.PI,1.5*Math.PI+this.health/101*2*Math.PI);//its health/101 because therre is a little gap between it
+   		ctx.stroke();
+   		ctx.closePath();
 	};
 }
