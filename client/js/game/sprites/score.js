319b81228e4800c82b136523a68793041f0fd8a6
Coen Stange <coenstange@me.com>
2013-01-04 20:57:18 +0100

    Moved score, because overlayed log

--- client/js/game/sprites/score.js
+++ client/js/game/sprites/score.js
@@ -8,14 +8,14 @@ function score() {
 		ctx.beginPath();
 		ctx.strokeStyle = "#DDDDDD";
 		ctx.lineWidth = 10;
-		ctx.arc(120,620,76,0,2*Math.PI);
+		ctx.arc(1160,620,76,0,2*Math.PI);
 		ctx.stroke();
 		ctx.closePath();
 		//draw health
 		ctx.beginPath();
 		ctx.strokeStyle = "#FF3B3B";
 		ctx.lineWidth = 8;
-		ctx.arc(120,620,76,1.5*Math.PI,1.5*Math.PI+gameRoom.health/101*2*Math.PI);//its health/101 because therre is a little gap between it
+		ctx.arc(1160,620,76,1.5*Math.PI,1.5*Math.PI+gameRoom.health/101*2*Math.PI);//its health/101 because therre is a little gap between it
 		ctx.stroke();
 		ctx.closePath();
 
@@ -23,14 +23,14 @@ function score() {
       ctx.fillStyle = '#000000';
       ctx.textAlign = 'right';
       ctx.font="48px Arial";
-      ctx.fillText(gameRoom.ammo,108,582);
+      ctx.fillText(gameRoom.ammo,1148,582);
 
       ctx.textAlign = 'center';
       ctx.font="80px Arial";
-      ctx.fillText("/",116,580);
+      ctx.fillText("/",1156,580);
 
       ctx.textAlign = 'left';
       ctx.font="42px Arial";
-      ctx.fillText(gameRoom.reloadAmmo,120,608);
+      ctx.fillText(gameRoom.reloadAmmo,1170,608);
 	};
 }
