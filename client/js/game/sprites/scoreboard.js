bfbffda95246fd23842f9f86658d079b741d8bbd
Coen Stange <coenstange@me.com>
2013-04-23 19:28:39 +0200

    Scoreboard sorts on k/d

--- client/js/game/sprites/scoreboard.js
+++ client/js/game/sprites/scoreboard.js
@@ -1,4 +1,3 @@
-
 var scoreBoard = {
 	cont:document.getElementById('score'),
 	addPlayer:function(name){
@@ -25,6 +24,10 @@ var scoreBoard = {
 		for (var name in scores) {
 			this.update(name,scores[name]);
 		};
+		
+		if(!ObIsEmpty(scores)){
+			SortTable(3);
+		}
 	},
 	update:function(name,score){
 		var row = document.getElementById("row"+name),
@@ -41,3 +44,36 @@ var scoreBoard = {
 		}
 	}
 };
+
+//sorteer tabbel
+function SortTable(sortOn) {
+	var table = document.getElementById('tbsb');
+	var tbody = table.getElementsByTagName('tbody')[0];
+	var rows = tbody.getElementsByTagName('tr');
+
+	var rowArray = new Array();
+	for (var i=0, length=rows.length; i<length; i++) {
+		rowArray[i] = new Object;
+		rowArray[i].oldIndex = i;
+		rowArray[i].value = rows[i].getElementsByTagName('td')[sortOn].firstChild.nodeValue;
+	}
+	rowArray.sort(RowCompareNumbers);
+
+	var newTbody = document.createElement('tbody');
+	for (var i=0, length=rowArray.length ; i<length; i++) {
+		newTbody.appendChild(rows[rowArray[i].oldIndex].cloneNode(true));
+	}
+
+	table.replaceChild(newTbody, tbody);
+}
+// Compare number
+function RowCompareNumbers(b, a) {
+	var aVal = parseInt( a.value);
+	var bVal = parseInt(b.value);
+	return (aVal - bVal);
+}
+
+function ObIsEmpty(ob){
+   for(var i in ob){ return false;}
+  return true;
+}
